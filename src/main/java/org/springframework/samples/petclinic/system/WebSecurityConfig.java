bc1673f5f2baa197a7117c6604d3378fac5c2b8d
leticia1404 <let.demoura@gmail.com>
2019-07-03 15:53:05 -0300

    Logout

--- src/main/java/org/springframework/samples/petclinic/system/WebSecurityConfig.java
+++ src/main/java/org/springframework/samples/petclinic/system/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/resources/**","/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
