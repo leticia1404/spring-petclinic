5cc12c42ec1f20d0449210481e7480fcbe993260
leticia <let.demoura@gmail.com>
2019-07-03 13:08:41 -0300

    #logout

--- src/main/java/org/springframework/samples/petclinic/system/WebSecurityConfig.java
+++ src/main/java/org/springframework/samples/petclinic/system/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("password")
                 .roles("USER")
                 .build();
-
+ 
         return new InMemoryUserDetailsManager(user);
     }
 }
