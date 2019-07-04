bd2964a12b5c027065c5ed0db69f63df81fb6d8f
leticia <let.demoura@gmail.com>
2019-07-04 12:02:45 -0300

    Logout funcionando

--- src/main/java/org/springframework/samples/petclinic/system/WebSecurityConfig.java
+++ src/main/java/org/springframework/samples/petclinic/system/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -25,7 +26,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .logout()
-                .permitAll();
+	            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+	            .logoutSuccessUrl("/")
+	            .permitAll();
     }
 
     @Bean
